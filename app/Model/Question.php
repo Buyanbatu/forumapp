c970846121f9ee0ea581f471dc5907c053b85a4a
Buyanbat <buyanbatu@gmail.com>
2019-09-30 16:55:46 -0700

    Initial Commit

---
+++ app/Model/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function user(){
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reply(){
+        return $this->hasMany(Reply::class);
+    }
+}
